e18834432d3867ef6ee68ca763f6d974c0eaa4aa
jin park <jinpark0625@gmail.com>
2023-08-17 19:22:11 -0400

    feat: Add test width

--- src/components/HeaderWrap/HeaderWrap.tsx
+++ src/components/HeaderWrap/HeaderWrap.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from '../Header/Header';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, Dimensions} from 'react-native';
 import {NativeStackHeaderProps} from '@react-navigation/native-stack';
 
 const HeaderWrap = ({...props}: NativeStackHeaderProps) => {
   return (
     <SafeAreaView className="bg-white dark:bg-black">
-      <Header {...props} />
+      <Header
+        animatingWidthValues={[0, Dimensions.get('window').width * 0.25]}
+        {...props}
+      />
     </SafeAreaView>
   );
 };
