9f141505cefcac0340fbd82511d74c8ffcae7324
jin park <jinpark0625@gmail.com>
2023-08-18 23:58:47 -0400

    chore: Update HeaderWrap Component for Route-Specific Configuration

--- src/components/HeaderWrap/HeaderWrap.tsx
+++ src/components/HeaderWrap/HeaderWrap.tsx
@@ -2,12 +2,34 @@ import React from 'react';
 import Header from '../Header/Header';
 import {SafeAreaView, Dimensions} from 'react-native';
 import {NativeStackHeaderProps} from '@react-navigation/native-stack';
+import {Text} from 'react-native';
 
 const HeaderWrap = ({...props}: NativeStackHeaderProps) => {
+  const {
+    route: {name},
+  } = props;
+
+  let animatingWidthValues;
+  let right;
+  let border;
+
+  if (name === 'Terms') {
+    animatingWidthValues = [0, Dimensions.get('window').width * 0.5];
+    right = <Text className="text-neutral-600">1 of 2</Text>;
+    border = true;
+  }
+  if (name === 'Register') {
+    animatingWidthValues = [0, Dimensions.get('window').width * 1];
+    right = <Text className="text-neutral-600">2 of 2</Text>;
+    border = true;
+  }
+
   return (
     <SafeAreaView className="bg-white dark:bg-black">
       <Header
-        animatingWidthValues={[0, Dimensions.get('window').width * 0.25]}
+        animatingWidthValues={animatingWidthValues}
+        right={right}
+        border={border}
         {...props}
       />
     </SafeAreaView>
