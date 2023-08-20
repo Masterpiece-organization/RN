91ff105117d37e0a4bad0e2b13e4535043efc2bc
jin park <jinpark0625@gmail.com>
2023-08-19 22:37:39 -0400

    refactor: Refactored HeaderWrap component styles and structure.

--- src/components/HeaderWrap/HeaderWrap.tsx
+++ src/components/HeaderWrap/HeaderWrap.tsx
@@ -1,33 +1,79 @@
 import React from 'react';
 import Header from '../Header/Header';
+import Text from '../Text';
 import {SafeAreaView, Dimensions} from 'react-native';
 import {NativeStackHeaderProps} from '@react-navigation/native-stack';
-import {Text} from 'react-native';
+import {useMainContext} from '@/context/MainContext';
 
 const HeaderWrap = ({...props}: NativeStackHeaderProps) => {
+  const contexts = useMainContext();
+
   const {
     route: {name},
   } = props;
 
   let animatingWidthValues;
+  let left;
+  let center;
   let right;
   let border;
 
-  if (name === 'Terms') {
+  /**
+   * TODO!!!! Simplify the code, deleting Text in center and right component
+   */
+
+  if (name === 'Terms' || name === 'FindPw') {
     animatingWidthValues = [0, Dimensions.get('window').width * 0.5];
-    right = <Text className="text-neutral-600">1 of 2</Text>;
+    left = true;
+    center = (
+      <Text
+        className={`${
+          contexts?.colorScheme === 'dark' ? 'text-white' : 'text-black'
+        }`}>
+        {name === 'Terms' ? '서비스 이용 동의 및 확인' : '비밀번호 재설정'}
+      </Text>
+    );
+    right = (
+      <Text
+        className={`${
+          contexts?.colorScheme === 'dark' ? 'text-white' : 'text-black'
+        }`}>
+        1 of 2
+      </Text>
+    );
     border = true;
   }
-  if (name === 'Register') {
+  if (name === 'Register' || name === 'ResetPw') {
     animatingWidthValues = [0, Dimensions.get('window').width * 1];
-    right = <Text className="text-neutral-600">2 of 2</Text>;
+    left = true;
+    center = (
+      <Text
+        className={`${
+          contexts?.colorScheme === 'dark' ? 'text-white' : 'text-black'
+        }`}>
+        {name === 'Register' ? '회원가입' : '비밀번호 재설정'}
+      </Text>
+    );
+    right = (
+      <Text
+        className={`${
+          contexts?.colorScheme === 'dark' ? 'text-white' : 'text-black'
+        }`}>
+        2 of 2
+      </Text>
+    );
     border = true;
+  }
+  if (name === 'RegisterSuccess') {
+    left = false;
   }
 
   return (
     <SafeAreaView className="bg-white dark:bg-black">
       <Header
         animatingWidthValues={animatingWidthValues}
+        left={left}
+        center={center}
         right={right}
         border={border}
         {...props}
