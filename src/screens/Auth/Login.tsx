31b74299d5835ab75931ab84dd4b60a48ddd2360
jin park <jinpark0625@gmail.com>
2023-08-19 22:41:38 -0400

    refactor: Refactored Login component styles and structure.

--- src/screens/Auth/Login.tsx
+++ src/screens/Auth/Login.tsx
@@ -1,35 +1,29 @@
-import React, {PropsWithChildren} from 'react';
-import {StackNavigationProp} from '@react-navigation/stack';
-import {SafeAreaView, Image, View, useColorScheme} from 'react-native';
-// import {themeColors} from '@/theme';
+import React from 'react';
+import {SafeAreaView, Image, View} from 'react-native';
 import {Button, Text, TextInput} from '@components/index';
+import {RootStackParamList} from '@/typings/RootStackParamList';
+import {StackScreenProps} from '@react-navigation/stack';
+import {useMainContext} from '@/context/MainContext';
 
-type RootStackParamList = {
-  Home: undefined;
-  Login: undefined;
-};
+let googleIcon = require('../../assets/icons/google.png');
+let appleIcon = require('../../assets/icons/apple.png');
+let kakaoIcon = require('../../assets/icons/kakao.png');
 
-type LoginScreenNavigationProp = StackNavigationProp<
-  RootStackParamList,
-  'Login'
->;
+const BUTTON_DATA = [googleIcon, appleIcon, kakaoIcon];
+type LoginScreenProps = StackScreenProps<RootStackParamList, 'Login'>;
 
-type Props = PropsWithChildren<{
-  navigation: LoginScreenNavigationProp;
-}>;
-
-const Login = ({navigation}: Props): JSX.Element => {
+const Login = ({navigation}: LoginScreenProps): JSX.Element => {
   const handleOnPress = () => {
     console.log('onPress!');
   };
 
-  const colorScheme = useColorScheme();
+  const contexts = useMainContext();
 
   return (
-    <SafeAreaView className="flex-1 bg-white dark:bg-black">
+    <SafeAreaView className="flex-1 ">
       <SafeAreaView className="flex">
         <View className="justify-center px-8 pt-8">
-          <Text classStyle="mb-2" type="title">
+          <Text className="mb-2" type="title">
             안녕하세요,
           </Text>
           <Text>회원 서비스 이용을 위해 로그인 해주세요.</Text>
@@ -38,60 +32,28 @@ const Login = ({navigation}: Props): JSX.Element => {
 
       <View className="flex-1 px-8 pt-9">
         <View className="flex-row justify-center gap-x-2">
-          <View className="flex-1">
-            <Button
-              label=""
-              onPress={handleOnPress}
-              type={colorScheme === 'dark' ? '' : 'outlined'}
-              buttonColor={
-                colorScheme === 'dark' ? 'bg-neutral-700' : 'bg-neutral-300'
-              }
-              icon={
-                <Image
-                  source={require('../../assets/icons/google.png')}
-                  className="w-6 h-6"
-                />
-              }
-            />
-          </View>
-          <View className="flex-1">
-            <Button
-              label=""
-              onPress={handleOnPress}
-              type={colorScheme === 'dark' ? '' : 'outlined'}
-              buttonColor={
-                colorScheme === 'dark' ? 'bg-neutral-700' : 'bg-neutral-300'
-              }
-              icon={
-                <Image
-                  source={require('../../assets/icons/apple.png')}
-                  className="w-6 h-6 blur"
-                />
-              }
-            />
-          </View>
-          <View className="flex-1">
-            <Button
-              label=""
-              onPress={handleOnPress}
-              type={colorScheme === 'dark' ? '' : 'outlined'}
-              buttonColor={
-                colorScheme === 'dark' ? 'bg-neutral-700' : 'bg-neutral-300'
-              }
-              icon={
-                <Image
-                  source={require('../../assets/icons/kakao.png')}
-                  className="w-6 h-6"
-                />
-              }
-            />
-          </View>
+          {BUTTON_DATA.map(source => (
+            <View className="flex-1">
+              <Button
+                label=""
+                onPress={handleOnPress}
+                type={contexts?.colorScheme === 'dark' ? 'primary' : 'outlined'}
+                buttonColor={
+                  contexts?.colorScheme === 'dark' ? 'bg-neutral-700' : ''
+                }
+                className="border-neutral-300"
+                icon={<Image source={source} className="w-6 h-6" />}
+              />
+            </View>
+          ))}
         </View>
         <View className="relative pt-16 flex items-center justify-center">
           <View className="absolute z-10 bg-white dark:bg-black px-3">
             <Text
               textColor={
-                colorScheme === 'dark' ? 'text-white' : 'text-neutral-600'
+                contexts?.colorScheme === 'dark'
+                  ? 'text-white'
+                  : 'text-neutral-600'
               }>
               또는
             </Text>
@@ -111,13 +73,13 @@ const Login = ({navigation}: Props): JSX.Element => {
             placeholderTextColor="#9ca3af"
             // error="에러메시지"
           />
-          <View>
+          <View className="pt-3">
             <Button
               label="로그인"
               onPress={handleOnPress}
               textColor="text-white"
               buttonColor={
-                colorScheme === 'dark' ? 'bg-neutral-700' : 'bg-black'
+                contexts?.colorScheme === 'dark' ? 'bg-neutral-700' : 'bg-black'
               }
             />
           </View>
@@ -126,10 +88,13 @@ const Login = ({navigation}: Props): JSX.Element => {
         <View className="mt-7 items-center">
           <Button
             label="비밀번호를 잊으셨나요?"
-            onPress={handleOnPress}
+            onPress={() => navigation.navigate('FindPw')}
             textColor={
-              colorScheme === 'dark' ? 'text-white' : 'text-neutral-600'
+              contexts?.colorScheme === 'dark'
+                ? 'text-white'
+                : 'text-neutral-600'
             }
+            buttonColor=""
             type="text"
           />
         </View>
@@ -139,18 +104,23 @@ const Login = ({navigation}: Props): JSX.Element => {
         <View className="flex-row justify-center items-center">
           <Text
             textColor={
-              colorScheme === 'dark' ? 'text-white' : 'text-neutral-600'
+              contexts?.colorScheme === 'dark'
+                ? 'text-white'
+                : 'text-neutral-600'
             }>
             아직 회원이 아니신가요?
           </Text>
           <Button
             label="회원가입"
-            onPress={handleOnPress}
+            onPress={() => navigation.navigate('Terms')}
             textColor={
-              colorScheme === 'dark' ? 'text-white' : 'text-neutral-900'
+              contexts?.colorScheme === 'dark'
+                ? 'text-white'
+                : 'text-neutral-900'
             }
             type="text"
-            customButtonStyle="pl-2"
+            buttonColor=""
+            className="pl-2"
           />
         </View>
       </View>
