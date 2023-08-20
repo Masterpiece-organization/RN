eeefaf68186937aed4dabdb863c70115c7a07c94
jin park <jinpark0625@gmail.com>
2023-08-19 22:38:45 -0400

    feat: Add new screens

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
 import {
   createNativeStackNavigator,
   NativeStackHeaderProps,
@@ -7,17 +7,33 @@ import {
 import Home from '@/screens/Home/Home';
 import Login from '@/screens/Auth/Login';
 import Register from '@/screens/Auth/Register';
+import RegisterSuccess from '@/screens/Auth/RegisterSuccess';
+import FindPw from '@/screens/Auth/FindPw';
+import ResetPw from '@/screens/Auth/ResetPw';
+import Terms from '@/screens/Auth/Terms';
 import {HeaderWrap} from '@/components';
+import {useMainContext} from '@/context/MainContext';
+import {RootStackParamList} from '@/typings/RootStackParamList';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 const headerWrapComponent = (props: NativeStackHeaderProps) => (
   <HeaderWrap {...props} />
 );
 
 const Navigation = () => {
+  const contexts = useMainContext();
+
+  const navTheme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      background: contexts?.colorScheme === 'dark' ? '#222' : 'white',
+    },
+  };
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Welcome">
+    <NavigationContainer theme={navTheme}>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen
           name="Home"
           options={{headerShown: false}}
@@ -25,16 +41,45 @@ const Navigation = () => {
         />
         <Stack.Screen
           name="Login"
-          // options={{headerShown: false}}
           options={{
             header: props => headerWrapComponent(props),
           }}
           component={Login}
         />
         <Stack.Screen
+          name="Terms"
+          options={{
+            header: props => headerWrapComponent(props),
+          }}
+          component={Terms}
+        />
+        <Stack.Screen
           name="Register"
-          options={{headerShown: false}}
+          options={{
+            header: props => headerWrapComponent(props),
+          }}
           component={Register}
+        />
+        <Stack.Screen
+          name="RegisterSuccess"
+          options={{
+            header: props => headerWrapComponent(props),
+          }}
+          component={RegisterSuccess}
+        />
+        <Stack.Screen
+          name="FindPw"
+          options={{
+            header: props => headerWrapComponent(props),
+          }}
+          component={FindPw}
+        />
+        <Stack.Screen
+          name="ResetPw"
+          options={{
+            header: props => headerWrapComponent(props),
+          }}
+          component={ResetPw}
         />
       </Stack.Navigator>
     </NavigationContainer>
