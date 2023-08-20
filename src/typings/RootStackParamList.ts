e81b2a9799a842a6989e348dccf743bcfc674f6e
jin park <jinpark0625@gmail.com>
2023-08-19 22:40:27 -0400

    feat: Add new route lists

---
+++ src/typings/RootStackParamList.ts
@@ -0,0 +1,9 @@
+export type RootStackParamList = {
+  Home: undefined;
+  Login: undefined;
+  Register: undefined;
+  Terms: undefined;
+  RegisterSuccess: undefined;
+  FindPw: undefined;
+  ResetPw: undefined;
+};
