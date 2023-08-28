2bac6993726ea46f8f4ffb29a9f76e854d2f5824
jin park <cjswoqkrwls@gmail.com>
2023-08-27 23:14:42 -0400

    remove: Delete obsolete code

--- src/components/PressableOpacity/PressableOpacity.types.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {PressableProps} from 'react-native';
-
-export interface PressableOpacityTypes extends PressableProps {
-  children: React.ReactNode;
-  isLoading?: boolean;
-  customStyle?: object | null;
-}
