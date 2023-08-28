c9953a3b255912701e7baa696d05a8760ad0efba
jin park <cjswoqkrwls@gmail.com>
2023-08-27 23:21:12 -0400

    refactor: Refactor FindPw component with react-hook-form and add timer

--- src/screens/Auth/FindPw.tsx
+++ src/screens/Auth/FindPw.tsx
@@ -1,18 +1,58 @@
 import {View, SafeAreaView} from 'react-native';
-import {Text, Button, TextInput} from '@components/index';
+import {useState, useCallback} from 'react';
+import {Text, Button, TextInput, Container} from '@components/index';
 import {RootStackParamList} from '@/typings/RootStackParamList';
 import {StackScreenProps} from '@react-navigation/stack';
-import {useMainContext} from '@/context/MainContext';
+import {useMainContext} from '@/contexts/MainContext';
+import {
+  useForm,
+  FormProvider,
+  SubmitHandler,
+  FieldValues,
+} from 'react-hook-form';
+import useTimer from '@/hooks/useTimer';
+import {useFocusEffect} from '@react-navigation/native';
 
 type FindPwScreenProps = StackScreenProps<RootStackParamList, 'FindPw'>;
 
 const FindPw = ({navigation}: FindPwScreenProps) => {
   const contexts = useMainContext();
 
+  // useForm hook and set default behavior/values
+  const {...methods} = useForm({mode: 'onSubmit'});
+
+  const [formError, setError] = useState<boolean>(false);
+  const [timerOn, setTimerOn] = useState<boolean>(false);
+
+  const {minutes, seconds} = useTimer({timerOn, setTimerOn});
+
+  const checkEmail: SubmitHandler<FieldValues> = data => {
+    // Process the submitted form data
+    console.log(data.email, data.authentication);
+
+    setTimerOn(true);
+  };
+
+  const onSubmit: SubmitHandler<FieldValues> = data => {
+    // Process the submitted form data
+    console.log(data.email, data.authentication);
+
+    navigation.navigate('ResetPw');
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      return () => {
+        setTimerOn(false);
+        methods.reset({});
+      };
+    }, []),
+  );
+
   return (
-    <SafeAreaView className="dark:bg-black">
+    <Container>
       <SafeAreaView className="flex">
-        <View className="justify-center px-8 pt-8 ">
+        <View className="justify-center pt-8 ">
           <Text className="mb-2" type="subtitle">
             비밀번호를 잊으셨나요?
           </Text>
@@ -20,31 +60,77 @@ const FindPw = ({navigation}: FindPwScreenProps) => {
         </View>
       </SafeAreaView>
 
-      <View className="px-8 pt-9">
+      <View className="pt-9">
         <View className="form space-y-2 -mt-2">
-          <TextInput
-            placeholder="이메일 주소"
-            placeholderTextColor="#9ca3af"
-            // error="에러메시지"
-          />
-          <TextInput
-            placeholder="인증번호"
-            placeholderTextColor="#9ca3af"
-            // error="에러메시지"
-          />
+          {formError ? (
+            <View>
+              <Text textColor="text-red">
+                문제가생겼습니다. 잠시후 다시 시도해주세요.
+              </Text>
+            </View>
+          ) : (
+            <>
+              <FormProvider {...methods}>
+                <View className="flex-row items-starti">
+                  <TextInput
+                    name="email"
+                    placeholder="이메일 주소"
+                    keyboardType="email-address"
+                    textContentType="emailAddress"
+                    rules={{
+                      required: '이메일을 입력해주세요.',
+                      pattern: {
+                        value: /\b[\w\\.+-]+@[\w\\.-]+\.\w{2,4}\b/,
+                        message: '이메일 형식이 아닙니다.',
+                      },
+                    }}
+                    setFormError={setError}
+                    className="mb-2 w-3/4"
+                    editable={timerOn ? false : true}
+                  />
+                  <Button
+                    label={
+                      timerOn
+                        ? `${minutes}:${seconds === 0 ? '00' : seconds}`
+                        : '인증요청'
+                    }
+                    onPress={methods.handleSubmit(checkEmail)}
+                    textColor="text-white"
+                    textSize="text-xs"
+                    className="flex-auto ml-2 h-12"
+                    isLoading={!timerOn && !!contexts?.isMutating}
+                    disabled={timerOn}
+                  />
+                </View>
+
+                {timerOn && (
+                  <TextInput
+                    name="verificationCode"
+                    placeholder="인증번호"
+                    keyboardType="number-pad"
+                    rules={{
+                      required: '인증번호를 입력해 주세요.',
+                    }}
+                    setFormError={setError}
+                    className="mb-2"
+                  />
+                )}
+              </FormProvider>
+            </>
+          )}
           <View className="pt-3">
-            <Button
-              label="다음"
-              onPress={() => navigation.navigate('ResetPw')}
-              textColor="text-white"
-              buttonColor={
-                contexts?.colorScheme === 'dark' ? 'bg-neutral-700' : 'bg-black'
-              }
-            />
+            {timerOn && (
+              <Button
+                label="확인"
+                onPress={methods.handleSubmit(onSubmit)}
+                textColor="text-white"
+                isLoading={timerOn && !!contexts?.isMutating}
+              />
+            )}
           </View>
         </View>
       </View>
-    </SafeAreaView>
+    </Container>
   );
 };
 
