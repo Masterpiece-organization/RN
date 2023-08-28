9e8c766e4e7a856de1e0b88f378cfed9ef6ce3b9
jin park <cjswoqkrwls@gmail.com>
2023-08-27 23:23:58 -0400

    content: Update text in Home

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -24,19 +24,10 @@ const Home = ({navigation}: HomeScreenProps) => {
           start={{x: 0.2, y: 0.4}}
           end={{x: 0.2, y: 0.9}}
           className="justify-end">
-          <SafeAreaView>
-            <View className="px-8 pt-8">
+          <SafeAreaView className="mx-5">
+            <View className="pt-8">
               <View className="justify-center">
                 <Text className="mb-2" textColor="text-white" type="title">
-                  {/* 축구 열정 가득한 경기를 원할 때는? "우리들만의 리그" 에서
-                함께하세요! */}
-                  {/* 가슴 설레는 승리의 기회,{'\n'}지금 바로{'\n'}우리들만의 리그에서 */}
-                  {/* 모두 함께 축구 열기에{'\n'}열광하는 우리들만의 리그 */}
-                  {/* 축구 열정 가득한{'\n'}경기를 원할 때는?{'\n'}우리들만의 리그
-                에서 {'\n'}함께하세요! */}
-                  {/* 모두가 하나 되어{'\n'}축구 열기에 열광하는 그곳, 우리들만의 리그 */}
-                  {/* 모두가 축구로{'\n'}하나가 되는 곳,{'\n'}우리들만의 리그 */}
-                  {/* 모두가 축구를 통해 하나되는 */}
                   축구로 하나가 되는{'\n'}우리들만의 리그
                 </Text>
                 <Text textColor="text-white">
@@ -44,7 +35,7 @@ const Home = ({navigation}: HomeScreenProps) => {
                 </Text>
               </View>
             </View>
-            <View className="px-8 pt-8">
+            <View className="py-8">
               <Button
                 label="시작하기"
                 onPress={() => navigation.navigate('Login')}
