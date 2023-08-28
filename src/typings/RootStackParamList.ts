7a6318495d739f56129ebf2714b4c53fecfec4e4
jin park <cjswoqkrwls@gmail.com>
2023-08-27 23:20:49 -0400

    feat: Update stack param lists types

--- src/typings/RootStackParamList.ts
+++ src/typings/RootStackParamList.ts
@@ -1,9 +1,16 @@
 export type RootStackParamList = {
   Home: undefined;
   Login: undefined;
-  Register: undefined;
+  Register: {email: string};
   Terms: undefined;
-  RegisterSuccess: undefined;
+  Success: {
+    name: string;
+  };
   FindPw: undefined;
   ResetPw: undefined;
+  CheckEmail: undefined;
+};
+
+export type MainRootStackParamList = {
+  Main: undefined;
 };
