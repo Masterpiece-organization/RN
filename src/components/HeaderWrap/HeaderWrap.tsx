a73eef095ca4cbeeea5a27e70de6c33123633256
jin park <cjswoqkrwls@gmail.com>
2023-08-27 23:14:27 -0400

    refactor: Simplify HeaderWrap component

--- src/components/HeaderWrap/HeaderWrap.tsx
+++ src/components/HeaderWrap/HeaderWrap.tsx
@@ -3,81 +3,68 @@ import Header from '../Header/Header';
 import Text from '../Text';
 import {SafeAreaView, Dimensions} from 'react-native';
 import {NativeStackHeaderProps} from '@react-navigation/native-stack';
-import {useMainContext} from '@/context/MainContext';
+
+type HeaderDataProps = {
+  [key: string]: {
+    left?: boolean;
+    border?: boolean;
+    right?: React.ReactNode;
+    animatingWidthValues?: number[];
+    center?: React.ReactNode;
+  };
+};
+
+const HEADER_DATA: HeaderDataProps = {
+  Terms: {
+    left: true,
+    border: true,
+    center: '서비스 이용 동의 및 확인',
+    animatingWidthValues: [0, Dimensions.get('window').width * 0.333],
+    right: <Text type="bodySmall">1 of 3</Text>,
+  },
+  CheckEmail: {
+    left: true,
+    border: true,
+    center: '이메일 확인',
+    animatingWidthValues: [0, Dimensions.get('window').width * 0.666],
+    right: <Text type="bodySmall">2 of 3</Text>,
+  },
+  Register: {
+    left: true,
+    border: true,
+    center: '회원가입',
+    animatingWidthValues: [0, Dimensions.get('window').width * 1],
+    right: <Text type="bodySmall">3 of 3</Text>,
+  },
+  FindPw: {
+    left: true,
+    border: true,
+    center: '비밀번호 재설정',
+    animatingWidthValues: [0, Dimensions.get('window').width * 0.5],
+    right: <Text type="bodySmall">1 of 2</Text>,
+  },
+  ResetPw: {
+    left: true,
+    border: true,
+    center: '비밀번호 재설정',
+    animatingWidthValues: [0, Dimensions.get('window').width * 1],
+    right: <Text type="bodySmall">2 of 2</Text>,
+  },
+  RegisterSuccess: {
+    left: true,
+  },
+};
 
 const HeaderWrap = ({...props}: NativeStackHeaderProps) => {
-  const contexts = useMainContext();
-
   const {
     route: {name},
   } = props;
 
-  let animatingWidthValues;
-  let left;
-  let center;
-  let right;
-  let border;
-
-  /**
-   * TODO!!!! Simplify the code, deleting Text in center and right component
-   */
-
-  if (name === 'Terms' || name === 'FindPw') {
-    animatingWidthValues = [0, Dimensions.get('window').width * 0.5];
-    left = true;
-    center = (
-      <Text
-        className={`${
-          contexts?.colorScheme === 'dark' ? 'text-white' : 'text-black'
-        }`}>
-        {name === 'Terms' ? '서비스 이용 동의 및 확인' : '비밀번호 재설정'}
-      </Text>
-    );
-    right = (
-      <Text
-        className={`${
-          contexts?.colorScheme === 'dark' ? 'text-white' : 'text-black'
-        }`}>
-        1 of 2
-      </Text>
-    );
-    border = true;
-  }
-  if (name === 'Register' || name === 'ResetPw') {
-    animatingWidthValues = [0, Dimensions.get('window').width * 1];
-    left = true;
-    center = (
-      <Text
-        className={`${
-          contexts?.colorScheme === 'dark' ? 'text-white' : 'text-black'
-        }`}>
-        {name === 'Register' ? '회원가입' : '비밀번호 재설정'}
-      </Text>
-    );
-    right = (
-      <Text
-        className={`${
-          contexts?.colorScheme === 'dark' ? 'text-white' : 'text-black'
-        }`}>
-        2 of 2
-      </Text>
-    );
-    border = true;
-  }
-  if (name === 'RegisterSuccess') {
-    left = false;
-  }
+  const headerData = HEADER_DATA[name];
 
   return (
     <SafeAreaView className="bg-white dark:bg-black">
-      <Header
-        animatingWidthValues={animatingWidthValues}
-        left={left}
-        center={center}
-        right={right}
-        border={border}
-        {...props}
-      />
+      <Header {...headerData} {...props} />
     </SafeAreaView>
   );
 };
