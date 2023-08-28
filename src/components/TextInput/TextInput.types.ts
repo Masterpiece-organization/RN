fddf372967ae0b6d14cc7d53b610159e47d1b0a1
jin park <cjswoqkrwls@gmail.com>
2023-08-27 23:07:45 -0400

    feat: Update TextInput component props type

--- src/components/TextInput/TextInput.types.ts
+++ src/components/TextInput/TextInput.types.ts
@@ -1,11 +1,21 @@
-export interface FormInputType {
-  type?: 'email' | 'password' | 'text';
+import {TextInputProps} from 'react-native';
+import {UseControllerProps} from 'react-hook-form';
+
+export interface FormInputType extends UseControllerProps, TextInputProps {
+  // type?: 'email' | 'password' | 'text';
+  // label?: string;
+  // error?: string;
+  // value?: string;
+  // onChangeText?: (text: string) => void;
+  // placeholderTextColor?: string;
+  // editable?: boolean;
+  // name: string;
+  // defaultValue?: string;
+  // setFormError: Function;
   label?: string;
-  error?: string;
-  value?: string;
-  onChangeText?: (text: string) => void;
-  placeholderTextColor?: string;
-  editable?: boolean;
+  name: string;
+  defaultValue?: string;
+  setFormError: Function;
 }
 
 export interface optionType {
