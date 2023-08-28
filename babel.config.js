d7be678baf913a1fb2e36a0a789487730765fdf5
jin park <cjswoqkrwls@gmail.com>
2023-08-27 23:01:26 -0400

    feat: Enable caching in Babel configuration

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,31 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        alias: {
-          '@components': './src/components',
-          '@': './src',
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@components': './src/components',
+            '@': './src',
+          },
         },
-      },
+      ],
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+      'nativewind/babel',
+      'react-native-reanimated/plugin',
     ],
-    'nativewind/babel',
-    'react-native-reanimated/plugin',
-  ],
+  };
 };
