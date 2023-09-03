df9f8ba8122c15744fcac3ed0f0ddbc5f066b1eb
jin park <jinpark0625@gmail.com>
2023-09-03 18:04:16 -0400

    feat: Pass AxiosInstance to API calls and handle token on login success

--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -7,6 +7,10 @@ import {
   verifyAuthCode,
   resetPassword,
 } from '@/api/auth';
+import {useApiContext} from '@/contexts/ApiContext';
+import {useMainContext} from '@/contexts/MainContext';
+import {AxiosInstance} from 'axios';
+import {setGenericPassword} from 'react-native-keychain';
 
 interface userProps {
   email: string;
@@ -22,44 +26,82 @@ interface authProps extends emailProps {
 }
 
 export default function useUser() {
+  const contexts = useApiContext();
+  const mainContexts = useMainContext();
+  // const authInstance = contexts?.authInstance;
+  const instance = contexts?.instance as AxiosInstance;
+
   const loginQuery = useMutation(
-    async (data: userProps) => await loginEmail(data.email, data.password),
+    async (data: userProps) => {
+      const {email, password} = data;
+      return await loginEmail({email, password, instance});
+    },
     {
-      onSuccess: res => console.log(res),
+      onSuccess: async res => {
+        const {access_token: accessToken, refresh_token: refreshToken} = res;
+
+        mainContexts?.setAuthState({
+          accessToken,
+          refreshToken,
+          authenticated: true,
+        });
+
+        await setGenericPassword(
+          'token',
+          JSON.stringify({
+            accessToken,
+            refreshToken,
+          }),
+        );
+      },
     },
   );
 
   const signUpQuery = useMutation(
-    async (data: userProps) => await signUp(data.email, data.password),
+    async (data: userProps) => {
+      const {email, password} = data;
+      await signUp({email, password, instance});
+    },
     {
       onSuccess: res => console.log(res),
     },
   );
 
   const checkEmailQuery = useMutation(
-    async (data: emailProps) => await checkEmail(data.email),
+    async (data: emailProps) => {
+      const {email} = data;
+      await checkEmail({email, instance});
+    },
     {
       onSuccess: res => res,
     },
   );
 
   const checkEmailPasswordQuery = useMutation(
-    async (data: emailProps) => await checkEmailPassword(data.email),
+    async (data: emailProps) => {
+      const {email} = data;
+      await checkEmailPassword({email, instance});
+    },
     {
       onSuccess: res => res,
     },
   );
 
   const verifyAuthCodeQuery = useMutation(
-    async (data: authProps) =>
-      await verifyAuthCode(data.email, data.auth_number),
+    async (data: authProps) => {
+      const {email, auth_number} = data;
+      await verifyAuthCode({email, auth_number, instance});
+    },
     {
       onSuccess: res => res,
     },
   );
 
   const resetPasswordQuery = useMutation(
-    async (data: userProps) => await resetPassword(data.email, data.password),
+    async (data: userProps) => {
+      const {email, password} = data;
+      await resetPassword({email, password, instance});
+    },
     {
       onSuccess: res => res,
     },
