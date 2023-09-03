5f633d677450a00f1c0b03578c3810e363c71bf9
jin park <jinpark0625@gmail.com>
2023-09-03 18:04:40 -0400

    feat: Add function to load JWT token at startup and manage loading status

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -2,9 +2,46 @@ import AuthStack from './AuthStack';
 import MainStack from './MainStack';
 import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
 import {useMainContext} from '@/contexts/MainContext';
+import {useCallback, useState, useEffect} from 'react';
+import {getGenericPassword} from 'react-native-keychain';
+import Loader from '@components/Loader';
 
 const Navigation = () => {
   const contexts = useMainContext();
+
+  const [status, setStatus] = useState('loading');
+
+  const loadJWT = useCallback(async () => {
+    try {
+      const value = await getGenericPassword();
+
+      if (value) {
+        const jwt = JSON.parse(value.password);
+
+        contexts?.setAuthState({
+          accessToken: jwt.accessToken || null,
+          refreshToken: jwt.refreshToken || null,
+          authenticated: jwt.accessToken !== null,
+        });
+        return setStatus('success');
+      }
+      setStatus('null');
+    } catch (err) {
+      const error = err as Error;
+      setStatus('error');
+      console.log(`Keychain Error: ${error?.message}`);
+      contexts?.setAuthState({
+        accessToken: null,
+        refreshToken: null,
+        authenticated: false,
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    loadJWT();
+  }, [loadJWT]);
 
   const navTheme = {
     ...DefaultTheme,
@@ -14,9 +51,17 @@ const Navigation = () => {
     },
   };
 
+  if (status === 'loading') {
+    return <Loader />;
+  }
+
   return (
     <NavigationContainer theme={navTheme}>
-      {contexts?.user ? <MainStack /> : <AuthStack />}
+      {contexts?.authState?.authenticated === false ? (
+        <AuthStack />
+      ) : (
+        <MainStack />
+      )}
     </NavigationContainer>
   );
 };
