c3bcf748a124943c93f4abdb959a56eb93243238
jin park <jinpark0625@gmail.com>
2023-09-03 18:03:45 -0400

    feat: Add position data

---
+++ src/data/position.ts
@@ -0,0 +1,27 @@
+export const positions = [
+  ['LW', 'ST', 'RW'],
+  ['CF'],
+  ['CAM'],
+  ['LM', 'CM', 'RM'],
+  ['LWB', 'CDM', 'RWB'],
+  ['LB', 'CB', 'RB'],
+  ['GK'],
+];
+
+export const positionsObj = [
+  {id: 1, position: 'GK'},
+  {id: 2, position: 'RB'},
+  {id: 3, position: 'RWB'},
+  {id: 4, position: 'CB'},
+  {id: 5, position: 'LB'},
+  {id: 6, position: 'LWB'},
+  {id: 7, position: 'CDM'},
+  {id: 8, position: 'RM'},
+  {id: 9, position: 'CM'},
+  {id: 10, position: 'LM'},
+  {id: 11, position: 'CAM'},
+  {id: 12, position: 'RW'},
+  {id: 13, position: 'ST'},
+  {id: 14, position: 'LW'},
+  {id: 15, position: 'CF'},
+];
