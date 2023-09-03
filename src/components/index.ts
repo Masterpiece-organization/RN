027ba27a7d71ae258f5ed092fef869f3e6436c0d
jin park <jinpark0625@gmail.com>
2023-09-03 18:01:41 -0400

    feat: Add PositionCheck component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,5 @@ export {default as TextInput} from './TextInput';
 export {default as Button} from './Button';
 export {default as HeaderWrap} from './HeaderWrap';
 export {default as CheckBox} from './CheckBox';
+export {default as PositionCheck} from './PositionCheck';
 export {default as Container} from './Container';
