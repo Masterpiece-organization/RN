2c01d9021bb3c20971488cfe7b515b93aecc20ea
jin park <jinpark0625@gmail.com>
2023-09-06 23:35:02 -0400

    feat: Add 'Button' component for logout functionality

--- src/screens/Main/Main.tsx
+++ src/screens/Main/Main.tsx
@@ -1,12 +1,16 @@
-import {Container, Text} from '@/components';
+import {Container, Text, Button} from '@/components';
 import {View} from 'react-native';
+import {useMainContext} from '@/contexts/MainContext';
 
 const Main = () => {
+  const contexts = useMainContext();
+
   return (
     <Container>
       <View>
         <Text>test</Text>
         <Text>test</Text>
+        <Button label="로그아웃" onPress={contexts?.logout || (() => {})} />
       </View>
     </Container>
   );
