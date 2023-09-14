2a1345c5e1c3311036c8ef8133fd29bb12202243
jin park <jinpark0625@gmail.com>
2023-09-13 23:57:00 -0400

    feat: Add focused property to icon and adjusted icon size

--- src/assets/icons/iconComponents/Team.tsx
+++ src/assets/icons/iconComponents/Team.tsx
@@ -4,18 +4,19 @@ import {SvgIconProps} from '@/typings/IconTypeList';
 const Team = ({
   fillColor = 'none',
   strokeColor = 'currentColor',
+  focused
 }: SvgIconProps) => {
   return (
     <Svg
       width="24px"
       height="24px"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
-      fill={fillColor}>
+      strokeWidth={focused ? 0 : 1.5}
+      fill={fillColor}
+      >
       <Path
         d="M7 18v-1a5 5 0 015-5v0a5 5 0 015 5v1M1 18v-1a3 3 0 013-3v0M23 18v-1a3 3 0 00-3-3v0M12 12a3 3 0 100-6 3 3 0 000 6zM4 14a2 2 0 100-4 2 2 0 000 4zM20 14a2 2 0 100-4 2 2 0 000 4z"
         stroke={strokeColor}
-        stroke-width="1.5"
         stroke-linecap="round"
         stroke-linejoin="round"
       />
