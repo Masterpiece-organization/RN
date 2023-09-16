825ee8cdc4c32cead319c31d385c99d9c68ef6fc
jin park <jinpark0625@gmail.com>
2023-09-15 23:54:40 -0400

    design: Fix styling issue in Team icon

--- src/assets/icons/iconComponents/Team.tsx
+++ src/assets/icons/iconComponents/Team.tsx
@@ -4,16 +4,19 @@ import {SvgIconProps} from '@/typings/IconTypeList';
 const Team = ({
   fillColor = 'none',
   strokeColor = 'currentColor',
-  focused
+  focused,
+  className,
+  width = 28,
+  height = 28,
 }: SvgIconProps) => {
   return (
     <Svg
-      width="24px"
-      height="24px"
+      width={width}
+      height={height}
       viewBox="0 0 24 24"
       strokeWidth={focused ? 0 : 1.5}
       fill={fillColor}
-      >
+      className={className}>
       <Path
         d="M7 18v-1a5 5 0 015-5v0a5 5 0 015 5v1M1 18v-1a3 3 0 013-3v0M23 18v-1a3 3 0 00-3-3v0M12 12a3 3 0 100-6 3 3 0 000 6zM4 14a2 2 0 100-4 2 2 0 000 4zM20 14a2 2 0 100-4 2 2 0 000 4z"
         stroke={strokeColor}
