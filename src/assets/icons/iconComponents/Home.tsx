2fb83c6bb2b0ba305fb70b488ffc29f76f66f4c3
jin park <jinpark0625@gmail.com>
2023-09-15 23:54:34 -0400

    design: Fix styling issue in Home icon

--- src/assets/icons/iconComponents/Home.tsx
+++ src/assets/icons/iconComponents/Home.tsx
@@ -4,16 +4,19 @@ import {SvgIconProps} from '@/typings/IconTypeList';
 const Home = ({
   fillColor = 'none',
   strokeColor = 'currentColor',
-  focused
+  focused,
+  className,
+  width = 28,
+  height = 28,
 }: SvgIconProps) => {
   return (
     <Svg
-      width={focused ? "26" : "24"}
-      height={focused ? "26" : "24"}
+      width={focused ? width + 2 : width}
+      height={focused ? height + 2 : height}
       strokeWidth="1.5"
       viewBox="0 0 24 24"
       fill={fillColor}
-      >
+      className={className}>
       <Path
         d="M9 21H7a4 4 0 01-4-4v-6.292a4 4 0 011.927-3.421l5-3.03a4 4 0 014.146 0l5 3.03A4 4 0 0121 10.707V17a4 4 0 01-4 4h-2m-6 0v-4a3 3 0 013-3v0a3 3 0 013 3v4m-6 0h6"
         stroke={strokeColor}
