d377b7b4ed0e4a197d8956700b2537109509ea55
jin park <jinpark0625@gmail.com>
2023-09-16 23:55:34 -0400

    refactor: Refactor HeaderWrap component to dynamically generate header data and add new headers

--- src/components/HeaderWrap/HeaderWrap.tsx
+++ src/components/HeaderWrap/HeaderWrap.tsx
@@ -3,67 +3,85 @@ import Header from '../Header/Header';
 import Text from '../Text';
 import {SafeAreaView, Dimensions} from 'react-native';
 import {NativeStackHeaderProps} from '@react-navigation/native-stack';
-
-type HeaderDataProps = {
-  [key: string]: {
-    left?: boolean;
-    border?: boolean;
-    right?: React.ReactNode;
-    animatingWidthValues?: number[];
-    center?: React.ReactNode;
-  };
-};
-
-const HEADER_DATA: HeaderDataProps = {
-  Terms: {
-    left: true,
-    border: true,
-    center: '서비스 이용 동의 및 확인',
-    animatingWidthValues: [0, Dimensions.get('window').width * 0.333],
-    right: <Text type="bodySmall">1 of 3</Text>,
-  },
-  CheckEmail: {
-    left: true,
-    border: true,
-    center: '이메일 확인',
-    animatingWidthValues: [0, Dimensions.get('window').width * 0.666],
-    right: <Text type="bodySmall">2 of 3</Text>,
-  },
-  Register: {
-    left: true,
-    border: true,
-    center: '회원가입',
-    animatingWidthValues: [0, Dimensions.get('window').width * 1],
-    right: <Text type="bodySmall">3 of 3</Text>,
-  },
-  FindPw: {
-    left: true,
-    border: true,
-    center: '비밀번호 재설정',
-    animatingWidthValues: [0, Dimensions.get('window').width * 0.5],
-    right: <Text type="bodySmall">1 of 2</Text>,
-  },
-  ResetPw: {
-    left: true,
-    border: true,
-    center: '비밀번호 재설정',
-    animatingWidthValues: [0, Dimensions.get('window').width * 1],
-    right: <Text type="bodySmall">2 of 2</Text>,
-  },
-  RegisterSuccess: {
-    left: true,
-  },
-};
+import Setting from '@/assets/icons/setting.svg';
+import {useMainContext} from '@/contexts/MainContext';
+import {HeaderDataType, GetHeaderDataType} from './HeaderWrap.type';
+import Button from '../Button';
 
 const HeaderWrap = ({...props}: NativeStackHeaderProps) => {
+  const contexts = useMainContext();
+
   const {
     route: {name},
+    navigation,
   } = props;
 
-  const headerData = HEADER_DATA[name];
+  const getHeaderData = ({
+    colorScheme,
+  }: GetHeaderDataType): Record<string, HeaderDataType> => ({
+    Terms: {
+      left: true,
+      border: true,
+      center: '서비스 이용 동의 및 확인',
+      animatingWidthValues: [0, Dimensions.get('window').width * 0.333],
+      right: <Text type="bodySmall">1 of 3</Text>,
+    },
+    CheckEmail: {
+      left: true,
+      border: true,
+      center: '이메일 확인',
+      animatingWidthValues: [0, Dimensions.get('window').width * 0.666],
+      right: <Text type="bodySmall">2 of 3</Text>,
+    },
+    Register: {
+      left: true,
+      border: true,
+      center: '회원가입',
+      animatingWidthValues: [0, Dimensions.get('window').width * 1],
+      right: <Text type="bodySmall">3 of 3</Text>,
+    },
+    FindPw: {
+      left: true,
+      border: true,
+      center: '비밀번호 재설정',
+      animatingWidthValues: [0, Dimensions.get('window').width * 0.5],
+      right: <Text type="bodySmall">1 of 2</Text>,
+    },
+    ResetPw: {
+      left: true,
+      border: true,
+      center: '비밀번호 재설정',
+      animatingWidthValues: [0, Dimensions.get('window').width * 1],
+      right: <Text type="bodySmall">2 of 2</Text>,
+    },
+    RegisterSuccess: {
+      left: true,
+    },
+    My: {
+      left: false,
+      border: false,
+      center: 'My',
+      right: (
+        <Button
+          label=""
+          onPress={() => navigation.navigate('Setting')}
+          buttonColor=""
+          className="h-6"
+          icon={<Setting color={colorScheme === 'dark' ? 'white' : 'black'} />}
+        />
+      ),
+    },
+    Setting: {
+      left: true,
+      border: false,
+      center: '설정',
+    },
+  });
+
+  const headerData = getHeaderData({colorScheme: contexts?.colorScheme})[name];
 
   return (
-    <SafeAreaView className="bg-white dark:bg-black">
+    <SafeAreaView className="bg-white dark:bg-black border-b border-neutral-300 dark:border-neutral-600">
       <Header {...headerData} {...props} />
     </SafeAreaView>
   );
