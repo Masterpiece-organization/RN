e36f4cbbffd4211353fc1d0cdf9e2c6fd57e9030
jin park <jinpark0625@gmail.com>
2023-09-16 23:57:44 -0400

    feat: Add Setting screen

--- src/screens/Main/index.ts
+++ src/screens/Main/index.ts
@@ -2,3 +2,4 @@ export {default as Main} from './Main';
 export {default as Team} from './Team';
 export {default as Matches} from './Matches';
 export {default as Profile} from './Profile';
+export {default as Setting} from './Setting';
