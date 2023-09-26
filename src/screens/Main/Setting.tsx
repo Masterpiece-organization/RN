b63f7f393e03a6ca385df1ddb458f848aa75af90
jin park <jinpark0625@gmail.com>
2023-09-25 23:49:57 -0400

    feat: Add Switch for Push Notifications and Refactor Profile Settings UI

--- src/screens/Main/Setting.tsx
+++ src/screens/Main/Setting.tsx
@@ -1,45 +1,64 @@
-import {View} from 'react-native';
-import {Text, Card, Button} from '@/components';
+import {useState} from 'react';
+import {View, Switch} from 'react-native';
+import {Card, Button} from '@/components';
 import ArrowRightIcon from '@/assets/icons/arrow_right.svg';
 import {useMainContext} from '@/contexts/MainContext';
-import {colorBasedOnTheme} from '@/theme';
+import {
+  colorBasedOnTheme,
+  defaultMargin,
+  elementBackgroundStyle,
+} from '@/theme';
 
 const Setting = () => {
   const contexts = useMainContext();
   const colorScheme = contexts?.colorScheme;
   const fillColor = colorBasedOnTheme(colorScheme, 'white', '#121212');
+  const logOut = contexts?.logout;
 
   const onPress = () => {
     console.log('test');
   };
 
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+  const SETTING_DATA = [
+    {
+      label: '자주 묻는 질문',
+      icon: <ArrowRightIcon color={fillColor} />,
+      onPress,
+      className: '',
+    },
+    {
+      label: '문의하기',
+      icon: <ArrowRightIcon color={fillColor} />,
+      onPress,
+      className: '',
+    },
+    {
+      label: '이용약관',
+      icon: <ArrowRightIcon color={fillColor} />,
+      onPress,
+      className: '',
+    },
+    {
+      label: '개인정보 처리방침',
+      icon: <ArrowRightIcon color={fillColor} />,
+      onPress,
+      className: '',
+    },
+    {
+      label: '버전정보',
+      icon: <ArrowRightIcon color={fillColor} />,
+      onPress,
+      className: 'mt-2 h-6',
+    },
+  ];
+
   return (
-    <View>
-      <Card title="프로필 설정">
-        <View className={`px-5`}>
-          <Button
-            label="닉네임 설정"
-            buttonColor=""
-            textColor=""
-            onPress={onPress}
-            buttonWrap="justify-between flex-1"
-            icon={<ArrowRightIcon color={fillColor} />}
-          />
-        </View>
-        <View className={`px-5`}>
-          <Button
-            label="포지션 수정"
-            buttonColor=""
-            textColor=""
-            className="mt-2 h-6"
-            onPress={onPress}
-            buttonWrap="justify-between flex-1"
-            icon={<ArrowRightIcon color={fillColor} />}
-          />
-        </View>
-      </Card>
-      <Card title="알림 설정">
-        <View className={`px-5`}>
+    <View className={`${defaultMargin}`}>
+      <Card title="알림 설정" className="rounded-lg">
+        <View className="-mr-1 px-5">
           <Button
             label="푸시알림 설정"
             buttonColor=""
@@ -49,7 +68,7 @@ const Setting = () => {
             icon={<ArrowRightIcon color={fillColor} />}
           />
         </View>
-        <View className={`px-5`}>
+        <View className="px-5">
           <Button
             label="방해금지 시간 설정 (보류)"
             buttonColor=""
@@ -57,23 +76,45 @@ const Setting = () => {
             className="mt-2 h-6"
             onPress={onPress}
             buttonWrap="justify-between flex-1"
-            icon={<ArrowRightIcon color={fillColor} />}
+            icon={
+              <Switch
+                trackColor={{false: '#767577', true: '#d4d4d4'}}
+                thumbColor={isEnabled ? '#121212' : '#f4f3f4'}
+                ios_backgroundColor="#3e3e3e"
+                onValueChange={toggleSwitch}
+                value={isEnabled}
+              />
+            }
           />
         </View>
       </Card>
 
-      <Card title="기타">
-        <View className={`px-5`}>
-          <Button
-            label="로그아웃"
-            buttonColor=""
-            textColor=""
-            className="mt-3 h-6"
-            onPress={contexts?.logout || (() => {})}
-            buttonWrap="justify-between flex-1"
-          />
-        </View>
+      <Card title="기타" className="rounded-lg">
+        {SETTING_DATA.map(({label, onPress, icon, className}) => {
+          return (
+            <View className="-mr-1 px-5">
+              <Button
+                label={label}
+                buttonColor=""
+                textColor=""
+                className={className}
+                onPress={onPress}
+                buttonWrap="justify-between flex-1"
+                icon={icon}
+              />
+            </View>
+          );
+        })}
       </Card>
+      <View className={`${elementBackgroundStyle} mt-4 rounded-lg px-5`}>
+        <Button
+          label="로그아웃"
+          buttonColor=""
+          textColor=""
+          onPress={logOut || (() => {})}
+          buttonWrap="justify-between flex-1"
+        />
+      </View>
     </View>
   );
 };
