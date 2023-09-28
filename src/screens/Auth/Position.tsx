23abf1d15e8a065b82c2249cfc92a1bb8ed34576
jin park <jinpark0625@gmail.com>
2023-09-27 23:35:58 -0400

    refactor: Refactor Position screen for improved readability and functionality

--- src/screens/Auth/Position.tsx
+++ src/screens/Auth/Position.tsx
@@ -1,19 +1,24 @@
 import {useState, useEffect} from 'react';
-import {
-  View,
-  SafeAreaView,
-  Dimensions,
-  TouchableWithoutFeedback,
-} from 'react-native';
+import {View, Dimensions, TouchableWithoutFeedback} from 'react-native';
 import {StackScreenProps} from '@react-navigation/stack';
 import {RootStackParamList} from '@/typings/RootStackParamList';
-import {Button, Text, PositionCheck, Container} from '@components/index';
+import {
+  Button,
+  PositionCheck,
+  Container,
+  TitleSection,
+} from '@components/index';
 import {useMainContext} from '@/contexts/MainContext';
 import {useForm, SubmitHandler, FieldValues} from 'react-hook-form';
 import FieldIcon from '@/assets/images/field.svg';
 import {positions, positionsObj} from '@/data/position';
 import showAlert from '@/utils/showAlert';
+import {defaultMargin} from '@/theme';
 
+interface ParamsTypes {
+  nickname?: string;
+  position?: string;
+}
 type PositionScreenProps = StackScreenProps<RootStackParamList, 'Position'>;
 
 const windowWidth = Dimensions.get('window').width;
@@ -23,17 +28,27 @@ const Position = ({navigation, route}: PositionScreenProps) => {
 
   const {...methods} = useForm({mode: 'onSubmit'});
 
-  const [param, setParam] = useState<string>('');
+  const [param, setParam] = useState<ParamsTypes>({
+    nickname: '',
+    position: '',
+  });
 
   const onSubmit: SubmitHandler<FieldValues> = data => {
     console.log(param, selectedPositions);
+
+    /**
+     * TODO: Add api
+     */
+    navigation.navigate('Success', {
+      name: 'register',
+    });
   };
 
   useEffect(() => {
-    if (route.params?.nickname) {
-      setParam(route.params?.nickname);
+    if (route.params) {
+      setParam(route.params);
     }
-  }, [route.params?.nickname]);
+  }, [route.params]);
 
   const handlePress = (position: string) => {
     const selectedId = positionsObj.find(pos => pos.position === position);
@@ -64,67 +79,63 @@ const Position = ({navigation, route}: PositionScreenProps) => {
   const [selectedPositions, setSelectedPositions] = useState<number[]>([]);
 
   return (
-    <Container scroll={true}>
-      <SafeAreaView className="flex">
-        <View className="justify-center pt-8 ">
-          <Text type="title" className="text-bold mb-3">
-            선호하는 포지션
-          </Text>
-          <Text
-            textColor={
-              contexts?.colorScheme === 'dark'
-                ? 'text-white'
-                : 'text-neutral-600'
-            }>
-            축구에서 가장 재미있게 뛸 수 있는 포지션을 골라보세요! 포지션은 최대
-            3개까지만 선택 가능합니다.
-          </Text>
-        </View>
-      </SafeAreaView>
+    <Container scroll={true} className={defaultMargin}>
+      <TitleSection
+        title="선호하는 포지션"
+        body="축구에서 가장 재미있게 뛸 수 있는 포지션을 골라보세요! 포지션은 최대
+        3개까지만 선택 가능합니다."
+      />
 
-      <View className="mt-9 relative w-full">
-        <View className="z-10 p-6" style={{height: (windowWidth - 40) * 1.618}}>
-          <View className="gap-5 justify-center flex-1">
-            {positions.map((row, rowIndex) => (
-              <View
-                key={rowIndex}
-                className={
-                  row.length > 1
-                    ? 'justify-between flex-row'
-                    : 'justify-center flex-row'
-                }>
-                {row.map(position => (
-                  <TouchableWithoutFeedback
-                    onPress={() => handlePress(position)}
-                    key={position}>
-                    <View>
-                      <PositionCheck
-                        text={position}
-                        checked={selectedPositions.includes(
-                          positionsObj.find(pos => pos.position === position)
-                            ?.id || 0,
-                        )}
-                      />
-                    </View>
-                  </TouchableWithoutFeedback>
-                ))}
-              </View>
-            ))}
+      <View className="mt-9">
+        <View className="relative w-full">
+          <View
+            className="z-10 p-6"
+            style={{height: (windowWidth - 40) * 1.618}}>
+            <View className="flex-1 justify-center gap-5">
+              {positions.map((row, rowIndex) => (
+                <View
+                  key={rowIndex}
+                  className={
+                    row.length > 1
+                      ? 'flex-row justify-between'
+                      : 'flex-row justify-center'
+                  }>
+                  {row.map(position => (
+                    <TouchableWithoutFeedback
+                      onPress={() => handlePress(position)}
+                      key={position}>
+                      <View>
+                        <PositionCheck
+                          text={position}
+                          checked={selectedPositions.includes(
+                            positionsObj.find(pos => pos.position === position)
+                              ?.id || 0,
+                          )}
+                        />
+                      </View>
+                    </TouchableWithoutFeedback>
+                  ))}
+                </View>
+              ))}
+            </View>
           </View>
-        </View>
-        <View
-          className="absolute z-0"
-          style={{width: windowWidth - 40, height: (windowWidth - 40) * 1.618}}>
-          <FieldIcon width="100%" height="100%" />
-        </View>
+          <View
+            className="absolute z-0"
+            style={{
+              width: windowWidth - 40,
+              height: (windowWidth - 40) * 1.618,
+            }}>
+            <FieldIcon width="100%" height="100%" />
+          </View>
 
-        <View className="pt-5 pb-8">
-          <Button
-            label="포지션 설정"
-            onPress={methods.handleSubmit(onSubmit)}
-            isLoading={!!contexts?.isMutating}
-            className="mb-2"
-          />
+          <View className="pb-8 pt-5">
+            <Button
+              label="포지션 설정"
+              onPress={methods.handleSubmit(onSubmit)}
+              isLoading={!!contexts?.isMutating}
+              className="mb-2"
+            />
+          </View>
         </View>
       </View>
     </Container>
