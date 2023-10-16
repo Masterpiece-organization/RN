8d969224a6bf3de594942263ec9d28314f748feb
jin park <jinpark0625@gmail.com>
2023-10-15 23:45:38 -0400

    refactor: Enhance Home Component with Animation and Blur Effects

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,57 +1,114 @@
-import React from 'react';
-import {View, SafeAreaView, ImageBackground, StyleSheet} from 'react-native';
+import {useEffect, useState} from 'react';
+import {
+  View,
+  SafeAreaView,
+  ImageBackground,
+  StyleSheet,
+  Dimensions,
+  Image,
+} from 'react-native';
 import {StackScreenProps} from '@react-navigation/stack';
 import {RootStackParamList} from '@/typings/RootStackParamList';
 import {Text, Button} from '@/components';
 import LinearGradient from 'react-native-linear-gradient';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  Easing,
+} from 'react-native-reanimated';
 
 export type HomeScreenProps = StackScreenProps<RootStackParamList, 'Home'>;
 
 const Home = ({navigation}: HomeScreenProps) => {
+  const {height} = Dimensions.get('window');
+
+  const textTranslateY = useSharedValue(height / 4);
+  const buttonOpacity = useSharedValue(0);
+  const imageScale = useSharedValue(1.5);
+
+  const textAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{translateY: textTranslateY.value}],
+    };
+  });
+
+  const contentsAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: buttonOpacity.value,
+    };
+  });
+
+  const imageAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{scale: imageScale.value}],
+    };
+  });
+
+  const [blurRadius, setBlurRadius] = useState(10);
+
+  useEffect(() => {
+    textTranslateY.value = withTiming(0, {
+      duration: 1000,
+      easing: Easing.ease,
+    });
+    buttonOpacity.value = withTiming(1, {
+      duration: 1000,
+      easing: Easing.ease,
+    });
+    imageScale.value = withTiming(1, {duration: 1000, easing: Easing.ease});
+    const interval = setInterval(() => {
+      setBlurRadius(prev => prev - 0.5);
+    }, 30);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <View>
-      <ImageBackground
-        source={require('../../assets/images/bg.jpg')}
+    <View className="flex-1 justify-between">
+      <SafeAreaView className="mx-lg mt-[136px]">
+        <Animated.View style={textAnimatedStyle}>
+          <Text
+            className="mb-md font-titleBold"
+            textColor="text-white"
+            type="title">
+            축구로 하나가 되는{'\n'}우리들만의 리그
+          </Text>
+        </Animated.View>
+        <Animated.View style={contentsAnimatedStyle}>
+          <Text type="subtitle" textColor="text-white">
+            지금 바로 가입하거나 로그인하여 즐거운{'\n'}축구 경험을 시작하세요!
+          </Text>
+        </Animated.View>
+      </SafeAreaView>
+
+      <SafeAreaView className="mx-lg ">
+        <Animated.View style={contentsAnimatedStyle}>
+          <View className="py-8">
+            <Button
+              label="로그인"
+              onPress={() => navigation.navigate('Login')}
+              buttonColor="bg-primary"
+              className="mb-xs"
+            />
+            <Button
+              label="회원가입"
+              onPress={() => navigation.navigate('Terms')}
+              textColor="text-black"
+              buttonColor="bg-white"
+            />
+          </View>
+        </Animated.View>
+      </SafeAreaView>
+
+      <Animated.Image
+        source={require('../../assets/images/onboarding.png')}
         resizeMode="cover"
-        className="w-full h-full">
-        <LinearGradient
-          colors={[
-            'rgba(255,255,255,0)',
-            'rgba(20,20,20,0.2)',
-            'rgba(20, 20, 20, .6)',
-          ]}
-          style={styles.linearGradient}
-          start={{x: 0.2, y: 0.4}}
-          end={{x: 0.2, y: 0.9}}
-          className="justify-end">
-          <SafeAreaView className="mx-5">
-            <View className="pt-8">
-              <View className="justify-center">
-                <Text className="mb-2" textColor="text-white" type="title">
-                  축구로 하나가 되는{'\n'}우리들만의 리그
-                </Text>
-                <Text textColor="text-white">
-                  지금 바로 가입하거나 로그인하여 즐거운 축구 경험을 시작하세요!
-                </Text>
-              </View>
-            </View>
-            <View className="py-8">
-              <Button
-                label="시작하기"
-                onPress={() => navigation.navigate('Login')}
-                buttonColor="bg-black"
-                className="my-4"
-              />
-              <Button
-                label="가입하기"
-                onPress={() => navigation.navigate('Terms')}
-                textColor="text-black"
-                buttonColor="bg-white"
-              />
-            </View>
-          </SafeAreaView>
-        </LinearGradient>
-      </ImageBackground>
+        className="absolute left-0 top-0 -z-10 h-full w-full"
+        style={imageAnimatedStyle}
+        blurRadius={blurRadius}
+        // blurRadius={imageBlurAnimatedStyle.opacity}
+      />
     </View>
   );
 };
@@ -63,3 +120,36 @@ var styles = StyleSheet.create({
     flex: 1,
   },
 });
+
+{
+  /* <Animated.View style={imageAnimatedStyle}> */
+}
+
+{
+  /* <ImageBackground
+          source={require('../../assets/images/onboarding.png')}
+          resizeMode="cover"
+          className="h-full w-full"> */
+}
+
+{
+  /* <LinearGradient
+        colors={[
+          'rgba(255,255,255,0)',
+          'rgba(20,20,20,0.2)',
+          'rgba(20, 20, 20, .6)',
+        ]}
+        style={styles.linearGradient}
+        start={{x: 0.2, y: 0.4}}
+        end={{x: 0.2, y: 0.9}}
+        className="justify-between"> */
+}
+{
+  /* </LinearGradient> */
+}
+{
+  /* </ImageBackground> */
+}
+{
+  /* </Animated.View> */
+}
