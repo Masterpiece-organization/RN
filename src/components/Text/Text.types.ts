fc7112a3107b4ad7f5bd2d2e473c4a9deb5b2006
jin park <jinpark0625@gmail.com>
2023-10-19 23:54:46 -0400

    style: Update Text component styles

--- src/components/Text/Text.types.ts
+++ src/components/Text/Text.types.ts
@@ -1,6 +1,13 @@
 export interface TextType {
   textColor?: string;
-  type?: 'title' | 'subtitle' | 'bodySmall' | 'small';
+  type?:
+    | 'title'
+    | 'subtitle'
+    | 'subtitleBold'
+    | 'subtitleSmall'
+    | 'bodySemi'
+    | 'bodySmall'
+    | 'small';
   className?: string;
   onPress?: () => void;
   children?: React.ReactNode | string;
