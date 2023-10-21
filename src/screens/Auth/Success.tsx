2236d68405c8c460b5fc02da73e2e8e477f2bc21
jin park <jinpark0625@gmail.com>
2023-10-20 23:53:23 -0400

    refactor: Refactoring Success Screen for Enhanced Readability and Navigation Flow

--- src/screens/Auth/Success.tsx
+++ src/screens/Auth/Success.tsx
@@ -1,10 +1,15 @@
-import {View, SafeAreaView} from 'react-native';
-import {Text, Button} from '@components/index';
-import {RootStackParamList} from '@/typings/RootStackParamList';
+import {Button, Container, TitleSection} from '@components/index';
+import {
+  RootStackParamList,
+  MainRootTabsParamList,
+} from '@/typings/RootStackParamList';
 import {StackScreenProps} from '@react-navigation/stack';
-import {useMainContext} from '@/contexts/MainContext';
+import {defaultMargin} from '@/theme';
 
-type SuccessScreenProps = StackScreenProps<RootStackParamList, 'Success'>;
+type SuccessScreenProps = StackScreenProps<
+  RootStackParamList & MainRootTabsParamList,
+  'Success'
+>;
 
 type SuccessDataTypes = {
   [key: string]: {
@@ -15,18 +20,16 @@ type SuccessDataTypes = {
 
 const SUCCESS_DATA: SuccessDataTypes = {
   resetPassword: {
-    title: '비밀번호가 성공적으로 변경되었습니다.',
-    body: '지금부터 변경된 비밀번호로 우리들만의 리그를 이용해 주시기 바랍니다.',
+    title: '새로운 비밀번호 설정이\n완료되었습니다!',
+    body: '지금바로 우리들의 리그를 이용해보세요.',
   },
   register: {
     title: '회원가입이 완료되었습니다!',
-    body: '지금 바로 우리들만의 리그를 이용해보세요.',
+    body: '지금바로 우리들의 리그를 이용해보세요.',
   },
 };
 
 const Success = ({navigation, route}: SuccessScreenProps) => {
-  const contexts = useMainContext();
-
   const {
     params: {name},
   } = route;
@@ -34,26 +37,22 @@ const Success = ({navigation, route}: SuccessScreenProps) => {
   const selectedSuccessData = SUCCESS_DATA[name];
 
   return (
-    <SafeAreaView className="dark:bg-black">
-      <SafeAreaView className="flex">
-        <View className="justify-center px-8 pt-8 ">
-          <Text className="mb-2" type="title">
-            {selectedSuccessData.title}
-          </Text>
-          <Text>{selectedSuccessData.body}</Text>
-        </View>
-      </SafeAreaView>
-      <View className="px-8 pt-9">
-        <Button
-          label="홈으로"
-          onPress={() => navigation.navigate('Home')}
-          textColor="text-white"
-          buttonColor={
-            contexts?.colorScheme === 'dark' ? 'bg-neutral-700' : 'bg-black'
-          }
-        />
-      </View>
-    </SafeAreaView>
+    <Container className={`${defaultMargin} justify-between`}>
+      <TitleSection
+        title={selectedSuccessData.title}
+        body={selectedSuccessData.body}
+      />
+      <Button
+        label="메인으로"
+        onPress={() => {
+          name === 'resetPassword'
+            ? navigation.navigate('Login')
+            : navigation.navigate('Main');
+        }}
+        textColor="text-white"
+        className="mb-lg"
+      />
+    </Container>
   );
 };
 
