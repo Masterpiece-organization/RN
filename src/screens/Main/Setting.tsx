deeb3130d1094ecdde8a8e4c4ffebc4633d5728c
jin park <jinpark0625@gmail.com>
2023-10-26 22:33:34 -0400

    style: Update UI design

--- src/screens/Main/Setting.tsx
+++ src/screens/Main/Setting.tsx
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 import {View, Switch} from 'react-native';
-import {Card, Button} from '@/components';
+import {Card, Button, Text} from '@/components';
 import ArrowRightIcon from '@/assets/icons/arrow_right.svg';
 import {useMainContext} from '@/contexts/MainContext';
 import {
@@ -25,96 +25,107 @@ const Setting = () => {
   const SETTING_DATA = [
     {
       label: '자주 묻는 질문',
-      icon: <ArrowRightIcon color={fillColor} />,
+      icon: <ArrowRightIcon width={16} color={fillColor} />,
       onPress,
-      className: '',
     },
     {
       label: '문의하기',
-      icon: <ArrowRightIcon color={fillColor} />,
+      icon: <ArrowRightIcon width={16} color={fillColor} />,
       onPress,
-      className: '',
     },
     {
       label: '이용약관',
-      icon: <ArrowRightIcon color={fillColor} />,
+      icon: <ArrowRightIcon width={16} color={fillColor} />,
       onPress,
-      className: '',
     },
     {
       label: '개인정보 처리방침',
-      icon: <ArrowRightIcon color={fillColor} />,
+      icon: <ArrowRightIcon width={16} color={fillColor} />,
       onPress,
-      className: '',
+    },
+    {
+      label: '테마',
+      icon: <ArrowRightIcon width={16} color={fillColor} />,
+      onPress,
     },
     {
       label: '버전정보',
-      icon: <ArrowRightIcon color={fillColor} />,
+      icon: null,
       onPress,
-      className: 'mt-2 h-6',
     },
   ];
 
   return (
-    <View className={`${defaultMargin}`}>
-      <Card title="알림 설정" className="rounded-lg">
-        <View className="-mr-1 px-5">
+    <View className={`${defaultMargin} mt-base`}>
+      <Card title="알림 설정" className="mb-xs space-x-5 space-y-4">
+        <View className="mr-sm">
           <Button
+            type="text"
             label="푸시알림 설정"
             buttonColor=""
             textColor=""
             onPress={onPress}
-            buttonWrap="justify-between flex-1"
-            icon={<ArrowRightIcon color={fillColor} />}
+            buttonWrap="justify-between"
+            icon={<ArrowRightIcon width={16} color={fillColor} />}
           />
         </View>
-        <View className="px-5">
+        <View className="mr-sm pb-sm">
           <Button
+            type="text"
             label="방해금지 시간 설정 (보류)"
             buttonColor=""
             textColor=""
-            className="mt-2 h-6"
             onPress={onPress}
-            buttonWrap="justify-between flex-1"
-            icon={
-              <Switch
-                trackColor={{false: '#767577', true: '#d4d4d4'}}
-                thumbColor={isEnabled ? '#121212' : '#f4f3f4'}
-                ios_backgroundColor="#3e3e3e"
-                onValueChange={toggleSwitch}
-                value={isEnabled}
-              />
-            }
+            buttonWrap="justify-between"
+            icon={<ArrowRightIcon width={16} color={fillColor} />}
+            // icon={
+            //   <Switch
+            //     trackColor={{false: '#767577', true: '#d4d4d4'}}
+            //     thumbColor={isEnabled ? '#121212' : '#f4f3f4'}
+            //     ios_backgroundColor="#3e3e3e"
+            //     onValueChange={toggleSwitch}
+            //     value={isEnabled}
+            //   />
+            // }
           />
         </View>
       </Card>
 
-      <Card title="기타" className="rounded-lg">
-        {SETTING_DATA.map(({label, onPress, icon, className}) => {
+      <Card title="기타" className="mb-xs space-x-5 space-y-4">
+        {SETTING_DATA.map(({label, onPress, icon}, index) => {
+          const isLastItem = index === SETTING_DATA.length - 1;
+
           return (
-            <View className="-mr-1 px-5">
+            <View
+              className={`mr-sm ${
+                isLastItem ? 'flex-row justify-between pb-sm' : ''
+              }`}>
               <Button
+                type="text"
                 label={label}
                 buttonColor=""
                 textColor=""
-                className={className}
                 onPress={onPress}
-                buttonWrap="justify-between flex-1"
+                buttonWrap="justify-between"
                 icon={icon}
               />
+              {isLastItem && <Text className="mr-1">v0.1.0</Text>}
             </View>
           );
         })}
       </Card>
-      <View className={`${elementBackgroundStyle} mt-4 rounded-lg px-5`}>
-        <Button
-          label="로그아웃"
-          buttonColor=""
-          textColor=""
-          onPress={logOut || (() => {})}
-          buttonWrap="justify-between flex-1"
-        />
-      </View>
+      <Card>
+        <View className="mx-sm py-sm">
+          <Button
+            type="text"
+            label="로그아웃"
+            buttonColor=""
+            textColor="text-dark-red"
+            onPress={logOut || (() => {})}
+            buttonWrap="justify-between"
+          />
+        </View>
+      </Card>
     </View>
   );
 };
