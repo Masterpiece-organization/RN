125b528e11d5debc5d2f50b723806ec1a768add0
jin park <jinpark0625@gmail.com>
2023-10-29 23:53:57 -0400

    style: Update UI design

--- src/components/RadioButton/RadioButton.tsx
+++ src/components/RadioButton/RadioButton.tsx
@@ -4,13 +4,13 @@ import useTheme from '@/hooks/useTheme';
 
 const RadioButton = ({selected, onPress}: RadioButtonType) => {
   const {borderColorTheme} = useTheme();
-  const {borderGrey} = borderColorTheme();
+  const {borderWhite} = borderColorTheme();
 
   return (
     <TouchableOpacity onPress={onPress}>
       <View
         className={`h-5 w-5 items-center justify-center rounded-full border ${
-          selected ? 'border-primary' : borderGrey
+          selected ? 'border-primary' : borderWhite
         }`}>
         {selected ? <View className="h-3 w-3 rounded-full bg-primary" /> : null}
       </View>
