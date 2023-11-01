f241db699cf2e0a4c73d6baf42b51fc9f4735925
jin park <jinpark0625@gmail.com>
2023-10-31 23:43:32 -0400

    refactor: Add textColortype, numberOfLines, and simplify styling logic in Text component

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,54 +1,68 @@
 import {Text as RNText} from 'react-native';
 import {TextType} from './Text.types';
 import {clsx} from 'clsx';
-import {useMainContext} from '@/contexts/MainContext';
+import useTheme from '@/hooks/useTheme';
 
 const Text = ({
   textColor = undefined,
+  textColortype = 'black',
   type,
   onPress,
   className,
+  numberOfLines,
   children,
 }: TextType): JSX.Element => {
-  const contexts = useMainContext();
+  const {textColorThemes} = useTheme();
+  const {black, dark} = textColorThemes();
 
-  let textType = defaultStyle.body;
+  let textType;
 
-  if (type === 'title') {
-    textType = defaultStyle.title;
+  switch (type) {
+    case 'title':
+      textType = defaultStyle.title;
+      break;
+    case 'subtitle':
+      textType = defaultStyle.subtitle;
+      break;
+    case 'subtitleBold':
+      textType = defaultStyle.subtitleBold;
+      break;
+    case 'subtitleSmall':
+      textType = defaultStyle.subtitleSmall;
+      break;
+    case 'bodySmall':
+      textType = defaultStyle.bodySmall;
+      break;
+    case 'bodySemi':
+      textType = defaultStyle.bodySemi;
+      break;
+    case 'small':
+      textType = defaultStyle.small;
+      break;
+    default:
+      textType = defaultStyle.body;
   }
 
-  if (type === 'subtitle') {
-    textType = defaultStyle.subtitle;
-  }
+  let defaultColor;
 
-  if (type === 'subtitleBold') {
-    textType = defaultStyle.subtitleBold;
+  switch (textColortype) {
+    case 'dark':
+      defaultColor = dark;
+      break;
+    case 'grey':
+      defaultColor = 'text-gray-600';
+      break;
+    default:
+      defaultColor = black;
   }
-
-  if (type === 'subtitleSmall') {
-    textType = defaultStyle.subtitleSmall;
-  }
-
-  if (type === 'bodySmall') {
-    textType = defaultStyle.bodySmall;
-  }
-
-  if (type === 'bodySemi') {
-    textType = defaultStyle.bodySemi;
-  }
-
-  if (type === 'small') {
-    textType = defaultStyle.small;
-  }
-
-  const defaultColor =
-    contexts?.colorScheme === 'dark' ? 'text-white' : 'text-black';
 
   const textStyle = clsx(textType, textColor ?? defaultColor, className);
 
   return (
-    <RNText onPress={onPress} className={textStyle}>
+    <RNText
+      onPress={onPress}
+      className={textStyle}
+      numberOfLines={numberOfLines}>
       {children && children}
     </RNText>
   );
@@ -60,7 +74,7 @@ const defaultStyle = {
   title: 'text-2xl font-titleBold leading-7',
   subtitle: 'text-base font-titleMedium leading-5',
   subtitleBold: 'text-lg font-titleBold',
-  subtitleSmall: 'text-base font-body',
+  subtitleSmall: 'text-base font-bodySemiBold',
   bodySemi: 'text-base font-bodySemiBold leading-[22px]',
   body: 'text-base font-bodyRegular leading-[22px]',
   bodySmall: 'text-sm font-bodyRegular',
